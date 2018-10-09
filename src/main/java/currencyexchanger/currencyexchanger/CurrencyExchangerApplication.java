fde768819ce91da6ed833d82d45206d29f1bab1f
JD <donimirski2@gmail.com>
2018-10-09 17:57:03 +0200

    abcd

---
+++ src/main/java/currencyexchanger/currencyexchanger/CurrencyExchangerApplication.java
@@ -0,0 +1,12 @@
+package currencyexchanger.currencyexchanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyExchangerApplication.class, args);
+    }
+}
---
+++ src/test/java/currencyexchanger/currencyexchanger/CurrencyExchangerApplicationTests.java
@@ -0,0 +1,16 @@
+package currencyexchanger.currencyexchanger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyExchangerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
