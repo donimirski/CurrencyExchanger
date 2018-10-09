c1c6c714e61861a650bda1ffb4d8c75de98f2c96
Filip_R <Filipras@vp.pl>
2018-10-09 18:41:07 +0200

    add comment

--- src/main/java/currencyexchanger/currencyexchanger/CurrencyExchangerApplication.java
+++ src/main/java/currencyexchanger/currencyexchanger/CurrencyExchangerApplication.java
@@ -9,5 +9,5 @@ public class CurrencyExchangerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CurrencyExchangerApplication.class, args);
     }
-//    comment sasafasf
+//    comment nr 1
 }
