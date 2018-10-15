d0fc0c05a8c2a66807f25ee48ceb450c353d0ea6
JD <donimirski2@gmail.com>
2018-10-15 20:04:27 +0200

    abcdf

--- src/main/java/currencyexchanger/currencyexchanger/model/NbpExchangeRateDownloader.java
+++ src/main/java/currencyexchanger/currencyexchanger/model/NbpExchangeRateDownloader.java
@@ -1,9 +1,31 @@
 package currencyexchanger.currencyexchanger.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
+@Component
 public class NbpExchangeRateDownloader {
 
-    private RestTemplate template; //ma pobrać kurs z api NBP w jsonie
+    private final static String BASE_URL = "http://api.nbp.pl/api/exchangerates/rates/a/";
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public NbpExchangeRateDownloader(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+
+    public String getRateInfoInJson(String currency, String date) {
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + currency + "/" + date +
+                "/?format=json", String.class);
+        String res = response.toString();
+        return res;
+    }
+
+
 
 }
